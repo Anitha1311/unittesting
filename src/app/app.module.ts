d8f183c6d52e2f1b97171ed97e4214da2f62d83c
Anitha Madanasekar <anithama@virtusa.com>
2018-07-20 11:50:03 +0530

    Added basic form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { IdFormComponent } from './id-form/id-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
